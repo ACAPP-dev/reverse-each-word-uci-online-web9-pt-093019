08fa6c52f5e40bd78346a8d424bc44ad68bb092d
Andrew Capp <acapp909@gmail.com>
2019-10-24 23:18:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,13 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  reversed_sentence = []
+  
   sentence_array = sentence.split
   sentence_array.collect do |word|
     #reversed_sentence << word.reverse
-    reversed_sentence << word.reverse
+    word.reverse
 
-  end
-  reversed_sentence.join(" ")
+  end.join(" ")
 
 end
 
